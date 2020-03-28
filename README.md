1006b8a414e8d6dd09158c51342f703569dcf18f
wurenhai <46084337@qq.com>
2020-03-28 18:28:25 +0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# InteractiveRun
-Run application into desktop session
+# Interactive Run
+当程序以系统服务方式部署时，让它创建的子进程运行在桌面环境下
+
+## 演示
+
+参考winsw
+
+```bash
+# 安装服务
+WinSW.NET4.exe install
+# 启动服务，这时会在桌面上打开子程序
+WinSW.NET4.exe start
+# 停止服务
+WinSW.NET4.exe stop
+# 卸载服务
+WinSW.NET4.exe uninstall
+```
+
---
+++ runi/main.cpp
@@ -0,0 +1,132 @@
+﻿// main.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <wtsapi32.h>
+#include <userenv.h>
+
+#pragma comment(lib, "Wtsapi32.lib")
+#pragma comment(lib, "Userenv.lib")
+
+class scoped_handle
+{
+private:
+    HANDLE handle;
+public:
+    scoped_handle()
+    {
+        this->handle = NULL;
+    }
+
+    ~scoped_handle()
+    {
+        if (this->handle != NULL)
+            CloseHandle(this->handle);
+    }
+    HANDLE get()
+    {
+        return this->handle;
+    }
+    PHANDLE ref()
+    {
+        return &this->handle;
+    }
+};
+
+class scoped_env
+{
+private:
+    LPVOID env;
+public:
+    scoped_env()
+    {
+        this->env = NULL;
+    }
+
+    ~scoped_env()
+    {
+        if (this->env != NULL)
+            DestroyEnvironmentBlock(this->env);
+    }
+    LPVOID get()
+    {
+        return this->env;
+    }
+    LPVOID* ref()
+    {
+        return &this->env;
+    }
+};
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    scoped_handle token;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, token.ref()))
+    {
+        return 1;
+    }
+
+    // 提升进程权限(SE_TCB_NAME)
+    TOKEN_PRIVILEGES tkp;
+    LookupPrivilegeValue(NULL, SE_TCB_NAME, &tkp.Privileges[0].Luid);
+    tkp.PrivilegeCount = 1;
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    if (!AdjustTokenPrivileges(token.get(), FALSE, &tkp, sizeof tkp, NULL, NULL))
+    {
+        return 2;
+    }
+
+    // 查询桌面会话访问令牌
+    scoped_handle primaryToken;
+    DWORD consoleSessionId = WTSGetActiveConsoleSessionId();
+    // MSDN指出调用该函数必须使用LocalSystem账户，并且拥有SE_TCB_NAME权限
+    if (!WTSQueryUserToken(consoleSessionId, primaryToken.ref()))
+    {
+        return 3;
+    }
+
+    // 获取桌面用户环境变量
+    scoped_env env;
+    if (!CreateEnvironmentBlock(env.ref(), primaryToken.get(), FALSE))
+    {
+        return 4;
+    }
+
+    WCHAR desk[32] = { 0 };
+    lstrcpy(desk, TEXT("winsta0\\default"));
+
+    STARTUPINFO si;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(STARTUPINFO);
+    si.lpDesktop = desk;
+    si.wShowWindow = SW_SHOWNORMAL;
+    si.dwFlags = STARTF_USESHOWWINDOW;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&pi, sizeof(pi));
+
+    // 在桌面环境中运行程序
+    if (!CreateProcessAsUser(primaryToken.get(),
+        NULL,
+        lpCmdLine,
+        NULL,
+        NULL,
+        FALSE,
+        NORMAL_PRIORITY_CLASS | CREATE_NEW_CONSOLE | CREATE_UNICODE_ENVIRONMENT,
+        env.get(),
+        NULL,
+        &si,
+        &pi))
+    {
+        return 5;
+    }
+
+    // 等待桌面进程终止
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    return 0;
+}
+
